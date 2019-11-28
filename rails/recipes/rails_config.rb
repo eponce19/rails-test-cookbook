c79fb1a001112e336a76eb3afa73cb9aac766d44
Evelin Ponce <evelin@kinedu.com>
2019-11-28 12:02:56 -0600

    update line

--- rails/recipes/rails_config.rb
+++ rails/recipes/rails_config.rb
@@ -1,6 +1,6 @@
 # Run an update on the box.
 execute "setup unicorn" do
-    Chef::Log.info("Install nodejs and start unicorn")
+    Chef::Log.info("yum install nodejs")
     command "sudo yum -y install nodejs"
     command "sudo /srv/www/unicorn_sample/shared/scripts/unicorn start"
 end
