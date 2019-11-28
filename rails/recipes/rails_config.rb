d451bbd2eca9e9531297a041340089be240fb766
Evelin Ponce <evelin@kinedu.com>
2019-11-28 11:44:39 -0600

    Delete line

--- rails/recipes/rails_config.rb
+++ rails/recipes/rails_config.rb
@@ -1,7 +1,6 @@
 # Run an update on the box.
 execute "setup unicorn" do
     Chef::Log.info("Install nodejs and start unicorn")
-    command "sudo yum install mysql-devel"
     command "sudo yum -y install nodejs"
     command "sudo /srv/www/unicorn_sample/shared/scripts/unicorn start"
 end
